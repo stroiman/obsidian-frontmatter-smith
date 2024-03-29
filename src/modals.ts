0aa001632dee981fa83b5e06168d15cd6b21644b
Peter Strøiman <peter@stroiman.com>
2024-03-29 15:43:47 +0100

    Create wrapper for a fuzzy-find suggester.

---
+++ src/modals.ts
@@ -0,0 +1,63 @@
+import { App, FuzzyMatch, FuzzySuggestModal, SuggestModal } from "obsidian";
+
+export class FuzzySuggester<T> extends FuzzySuggestModal<T> {
+	items: T[];
+	getText: (item: T) => string;
+	itemSelected: boolean;
+	callback: (value: T | null) => void;
+
+	constructor(
+		app: App,
+		items: T[],
+		getText: (item: T) => string,
+		callback: (value: T | null) => void,
+	) {
+		super(app);
+		this.getText = getText;
+		this.itemSelected = false;
+		this.items = items;
+		this.callback = callback;
+	}
+
+	getItems(): T[] {
+		return this.items;
+	}
+
+	getItemText(item: T): string {
+		return this.getText(item);
+	}
+
+	onClose() {
+		if (!this.itemSelected) {
+			this.callback(null);
+		}
+	}
+
+	selectSuggestion(
+		value: FuzzyMatch<T>,
+		evt: MouseEvent | KeyboardEvent,
+	): void {
+		this.itemSelected = true;
+		console.log("Selected");
+		this.close();
+		this.onChooseItem(value.item, evt);
+		// this.callback(value.item);
+	}
+
+	onChooseItem(item: T, evt: MouseEvent | KeyboardEvent): void {
+		this.callback(item);
+	}
+}
+
+export class Suggester {
+	app: App;
+	constructor(app: App) {
+		this.app = app;
+	}
+
+	suggest(items: string[]): Promise<string | null> {
+		return new Promise((resolve, reject) => {
+			new FuzzySuggester<string>(this.app, items, (x) => x, resolve).open();
+		});
+	}
+}
