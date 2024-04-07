4881ac338def114f8dbd95862d6340c8db1b53fe
Peter Strøiman <peter@stroiman.com>
2024-04-07 14:31:04 +0200

    Explicitly focus input element in text modal.

--- src/ObsidianPromptModal.ts
+++ src/ObsidianPromptModal.ts
@@ -1,70 +1,71 @@
 import {
-	App,
-	ButtonComponent,
-	Modal,
-	Platform,
-	TextAreaComponent,
-	TextComponent,
+  App,
+  ButtonComponent,
+  Modal,
+  Platform,
+  TextAreaComponent,
+  TextComponent,
 } from "obsidian";
 
 export type PromptOptions = {
-	prompt: string;
-	defaultValue?: string;
-	multiLine?: boolean;
+  prompt: string;
+  defaultValue?: string;
+  multiLine?: boolean;
 };
 
 export class ObsidianPromptModal extends Modal {
-	private submitted = false;
-	private value: string;
-	private prompt: string;
-	private defaultValue?: string;
-	private cb: (value: string | null) => void;
+  private submitted = false;
+  private value: string;
+  private prompt: string;
+  private defaultValue?: string;
+  private cb: (value: string | null) => void;
 
-	constructor(
-		app: App,
-		options: PromptOptions,
-		cb: (value: string | null) => void,
-	) {
-		super(app);
-		this.prompt = options.prompt;
-		this.defaultValue = options.defaultValue;
-		this.cb = cb;
-	}
+  constructor(
+    app: App,
+    options: PromptOptions,
+    cb: (value: string | null) => void,
+  ) {
+    super(app);
+    this.prompt = options.prompt;
+    this.defaultValue = options.defaultValue;
+    this.cb = cb;
+  }
 
-	onOpen(): void {
-		this.titleEl.setText(this.prompt);
-		this.titleEl.id = "frontmatter-forge-prompt-label";
-		this.createForm();
-	}
+  onOpen(): void {
+    this.titleEl.setText(this.prompt);
+    this.titleEl.id = "frontmatter-forge-prompt-label";
+    this.createForm();
+  }
 
-	onClose(): void {
-		this.contentEl.empty();
-		if (!this.submitted) {
-			this.cb(null);
-		}
-	}
+  onClose(): void {
+    this.contentEl.empty();
+    if (!this.submitted) {
+      this.cb(null);
+    }
+  }
 
-	createForm(): void {
-		const form = this.contentEl.createDiv().createEl("form");
-		form.addEventListener("submit", (e) => {
-			e.preventDefault();
-			this.submitted = true;
-			this.close();
-			this.cb(this.value);
-		});
-		form.addClass("templater-prompt-div");
-		const textInput = new TextComponent(form);
-		textInput.inputEl.style.setProperty("width", "100%");
-		textInput.inputEl.setAttribute(
-			"aria-labelledby",
-			"frontmatter-forge-prompt-label",
-		);
+  createForm(): void {
+    const form = this.contentEl.createDiv().createEl("form");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.submitted = true;
+      this.close();
+      this.cb(this.value);
+    });
+    form.addClass("templater-prompt-div");
+    const textInput = new TextComponent(form);
+    textInput.inputEl.style.setProperty("width", "100%");
+    textInput.inputEl.setAttribute(
+      "aria-labelledby",
+      "frontmatter-forge-prompt-label",
+    );
 
-		this.value = this.defaultValue || "";
-		textInput.setPlaceholder("Type text here");
-		textInput.setValue(this.value);
-		textInput.onChange((value) => (this.value = value));
-	}
+    this.value = this.defaultValue || "";
+    textInput.setPlaceholder("Type text here");
+    textInput.setValue(this.value);
+    textInput.onChange((value) => (this.value = value));
+    textInput.inputEl.focus();
+  }
 }
 
 export type ObsidianPromptModalConstructor = typeof ObsidianPromptModal;
