88b4f688679979d2ce2782cee3755aba6ea2cf56
Peter Strøiman <peter@stroiman.com>
2024-08-17 12:43:12 +0200

    Add documentation of deepState

--- src/configuration-editor/helpers.ts
+++ src/configuration-editor/helpers.ts
@@ -27,16 +27,40 @@ type DeepState<T extends object> = {
   [P in keyof T]: State<T[P]>;
 };
 
+/**
+ * Given a van state carrying an object, create a new object with the same keys,
+ * but each value is a state object itself.
+ *
+ * Changes to those state objects will be propagated up to the original state.
+ *
+ * Note that state changes are only propagated from child to parent. We don't
+ * listen for state changes coming from the caller of this function
+ */
 export const deepState = <T extends object>(x: State<T>): DeepState<T> => {
+  // Gets the original _entries_ of the object. And entry is a 2-element array
+  // in the form if [key, value]
   const entries = Object.entries(x.val);
+  // For each value, create a state object of the value, resulting in a new set
+  // of _entries_ for the resulting object.
   const stateEntries = entries.map(([key, val]) => [key, van.state(val)]);
   const result = Object.fromEntries(stateEntries);
   van.derive(() => {
+    // Event listener for when the child states change.
+    // Construct a new array, containing the entries, but with a new boolean
+    // value indicating if this particular prop has changed.
     const data = stateEntries.map(([key, state]: any) => {
       const val = state.val;
-      return [key, val, val !== state.oldVal];
+      return [key, val, val !== state.oldVal /* was this value changed */];
     });
-    const changed = data.reduce((p, c) => p || c[2], false);
+    // Finds if _any_ value was changed.
+    // Why does this work?
+    // `Array.prototype.find` returns the first _truthy_ element, and the
+    // predicate looks up the value that tells if that particular state was
+    // changed. If such a field contains `true`, that `true` value is the result
+    // of `find`. If none is found, `undefined` is returned; which is `falsy`.
+    // Thus this implementation will return either `true` or `undefined`, which
+    // is good enough for the branch
+    const changed = data.find((x) => x[2]);
     if (changed) {
       x.val = Object.fromEntries(data);
     }
