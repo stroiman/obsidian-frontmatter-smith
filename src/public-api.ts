6a2e1013f07754d4c0a2f481f426078fd34e232a
Peter Strøiman <peter@stroiman.com>
2024-08-27 12:41:17 +0200

    Create simple public api
    This can be accessed by other plugins, or templater templates.

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,5 +1,53 @@
-export default class FrontmatterSmithAPI {
-  test() {
-    console.log("Test");
+import { createForgeFromConfig, TestFileManager } from "./Forge";
+import { Modals } from "./modals";
+import { PluginConfiguration } from "./plugin-configuration";
+import { ForgeConfiguration } from "./smith-configuration-schema";
+
+class ForgeAPI<TFile, TFileManager extends TestFileManager<TFile>> {
+  constructor(
+    private deps: {
+      fileManager: TFileManager;
+      suggester: Modals;
+      forgeConfiguration: ForgeConfiguration;
+    },
+  ) {}
+
+  async runOnFile(file: TFile) {
+    const forge = createForgeFromConfig(this.deps);
+    await forge.run(file);
+  }
+}
+
+export default class FrontmatterSmithAPI<
+  TFile,
+  TFileManager extends TestFileManager<TFile>,
+> {
+  deps: {
+    fileManager: TFileManager;
+    modals: Modals;
+    getConfig: () => PluginConfiguration;
+  };
+
+  constructor(deps: {
+    fileManager: TFileManager;
+    modals: Modals;
+    getConfig: () => PluginConfiguration;
+  }) {
+    this.deps = deps;
+  }
+
+  findForgeByName(name: string) {
+    const config = this.deps.getConfig();
+    const forgeConfiguration = config.smithConfiguration.forges.find(
+      (x) => x.name === name,
+    );
+    return (
+      forgeConfiguration &&
+      new ForgeAPI({
+        fileManager: this.deps.fileManager,
+        suggester: this.deps.modals,
+        forgeConfiguration,
+      })
+    );
   }
 }
