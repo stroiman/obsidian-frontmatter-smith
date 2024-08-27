64427ba9b246c57e062808bd945d60ff24831cb9
Peter Strøiman <peter@stroiman.com>
2024-08-27 12:13:44 +0200

    Remove unnecessay abstraction over base class
    The abstraction was to allow the class to be testable; but this class is
    not tested; the logic is extracted into other testable classes

--- main.ts
+++ main.ts
@@ -1,72 +1,64 @@
-import { Editor, MarkdownView, Plugin, PluginManifest, App } from "obsidian";
+import { Editor, MarkdownView, Plugin } from "obsidian";
 import { FuzzySuggester } from "src/FuzzySuggester";
 import { ObsidianPromptModal } from "src/ObsidianPromptModal";
 import { Modals } from "src/modals";
-import SettingTab from "./PluginSettings";
+import FrontmatterSmithSettingTab from "./PluginSettings";
 import {
   PluginConfiguration,
   parseConfigurationOrDefault,
 } from "src/plugin-configuration";
 import RootRunner from "src/RootRunner";
 
-interface PluginConstructor {
-  new (app: App, manifest: PluginManifest): Plugin;
-}
-
-//declare module "Obsidian" {
-//  declare var Plugin: PluginConstructor;
-//}
-
-class SmithConfigurationAPI {
+class FrontmatterSmithAPI {
   test() {
     console.log("Test");
   }
 }
 
-const createPluginClass = (baseClass: PluginConstructor) => {
-  return class MyPlugin extends baseClass {
-    settings: PluginConfiguration;
-    api: SmithConfigurationAPI;
+export default class FrontmatterSmithPlugin extends Plugin {
+  settings: PluginConfiguration;
+  api: FrontmatterSmithAPI;
 
-    async loadSettings() {
-      const storedSettings = await this.loadData();
-      this.settings = parseConfigurationOrDefault(storedSettings);
-    }
+  async loadSettings() {
+    const storedSettings = await this.loadData();
+    this.settings = parseConfigurationOrDefault(storedSettings);
+  }
 
-    async onload() {
-      await this.loadSettings();
-      this.api = new SmithConfigurationAPI();
+  async onload() {
+    await this.loadSettings();
+    this.api = new FrontmatterSmithAPI();
 
-      const handleSettingChange = (newValue: PluginConfiguration) => {
-        this.settings = newValue;
-        settingsTab.setValue(newValue);
-        this.saveData(newValue);
-      };
+    const handleSettingChange = (newValue: PluginConfiguration) => {
+      this.settings = newValue;
+      settingsTab.setValue(newValue);
+      this.saveData(newValue);
+    };
 
-      const settingsTab = new SettingTab(this.app, this, handleSettingChange);
-      settingsTab.setValue(this.settings);
-      this.addSettingTab(settingsTab);
+    const settingsTab = new FrontmatterSmithSettingTab(
+      this.app,
+      this,
+      handleSettingChange,
+    );
+    settingsTab.setValue(this.settings);
+    this.addSettingTab(settingsTab);
 
-      this.addCommand({
-        id: "forge-frontmatter",
-        name: "Forge frontmatter",
-        editorCallback: async (editor: Editor, view: MarkdownView) => {
-          const file = view.file;
-          if (file) {
-            const worker = new RootRunner(
-              this.settings.smithConfiguration,
-              this.app.fileManager,
-              new Modals(this.app, {
-                FuzzySuggester,
-                ObsidianPromptModal,
-              }),
-            );
-            await worker.run(file);
-          }
-        },
-      });
-    }
-  };
-};
-
-export default createPluginClass(Plugin as PluginConstructor);
+    this.addCommand({
+      id: "forge-frontmatter",
+      name: "Forge frontmatter",
+      editorCallback: async (editor: Editor, view: MarkdownView) => {
+        const file = view.file;
+        if (file) {
+          const worker = new RootRunner(
+            this.settings.smithConfiguration,
+            this.app.fileManager,
+            new Modals(this.app, {
+              FuzzySuggester,
+              ObsidianPromptModal,
+            }),
+          );
+          await worker.run(file);
+        }
+      },
+    });
+  }
+}
